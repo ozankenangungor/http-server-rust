4bdafa8ccdb5ae7efecd3e7648507e3a55cf1cfd
Ozan Kenan Güngör <s220204044@ankarabilim.edu.tr>
2026-07-18 11:30:00 +0300

    respond with 200 OK to every request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("failed to bind to port 4221");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => println!("accepted new connection"),
+            Ok(mut stream) => {
+                let mut buf = [0; 1024];
+                let _ = stream.read(&mut buf);
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").expect("failed to write response");
+            }
             Err(e) => eprintln!("error: {e}"),
         }
     }
